e3a9fd28c543e83eb425cbc5b3f2d924f9271edc
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2024-03-17 22:28:51 -0500

    updated to neuro for interview

--- pages/👔_Interview_Practice.py
+++ pages/👔_Interview_Practice.py
@@ -202,8 +202,8 @@ if "last_response_interview" not in st.session_state:
 if check_password2():
     st.info("Have fun. Enter responses at the bottom of the page or choose the Microphone option. This tool uses openai's GPT3.5 turbo 16k model.")
     system_context = st.radio("Select an interviewer type :", ("Tough", "Nice",), horizontal = True, index=0)
-    specialty = st.text_input("Enter your specialty", placeholder="e.g. Emergency Medicine")
-    position = st.text_input("Enter your position", placeholder="e.g. resident")
+    specialty = st.text_input("Enter your specialty", placeholder="e.g. Stroke Neurology")
+    position = st.text_input("Enter your sought position", placeholder="e.g. resident, faculty, administrator")
     
     
 
