7363b2f7abea62c90e58464a2c42fc1433ac87b1
David Liebovitz <DavidL@northwestern.edu>
2024-03-20 13:03:46 -0500

    misc

--- pages/📝_Capture_Data_from_Notes.py
+++ pages/📝_Capture_Data_from_Notes.py
@@ -422,7 +422,7 @@ if st.secrets["use_docker"] == "True" or check_password():
     st.set_page_config(page_title='Neurology Parser Assistant', layout = 'centered', page_icon = ':stethoscope:', initial_sidebar_state = 'auto')
     st.title("Neurology Parser Assistant")
     st.warning("""Who likes EHR click? What if AI could recognize all concepts and file them where they belong in the chart? This tool illustrates
-               progress in that direction.""")
+               progress in that direction with a variety of methods. Soon, one or more will meet muster for research or clinical use!""")
     disclaimer = """**Disclaimer:** This is a tool to assist chart abstraction for cancer related diagnoses. \n 
 2. This tool is not a real doctor. \n    
 3. You will not take any medical action based on the output of this tool. \n   
