1fa4ec31940a3c8da2a3cd30b6b0bca6fe0d9d8a
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2024-04-12 08:48:32 -0500

    goofed up

--- not_used/updated_PDF.py
+++ not_used/updated_PDF.py
@@ -132,7 +132,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"] or st.session_state["password2"] == st.secrets["password2"]:
+        if st.session_state["password"] == st.secrets["password"] or st.session_state["password"] == st.secrets["password2"]:
             st.session_state["password_correct"] = True
             # del st.session_state["password"]  # don't store password
         else:
